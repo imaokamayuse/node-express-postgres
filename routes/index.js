c6324ffd9dae1c61deb6766143a819be8ed48288
nirochan <166688516+nirochan@users.noreply.github.com>
2024-07-11 06:35:48 +0000

    a

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+let todos = [];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ToDo App' });
+router.get('/', function(req, res, next) 
+{
+  res.render('index', {
+     title: 'ToDo App' ,
+     todos: todos,
+    });
 });
 
+router.post('/', function(req, res, next) {
+  const todo = req.body.add;
+  todos.push(todo);
+  res.render('/');
+});
 module.exports = router;
